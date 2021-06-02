f48907d5207e64d1702043cad7476f7e3537ea3d
gaoyang <15195989321@163.com>
2021-06-03 01:43:01 +0800

    210603

--- lianxicode/test_start_selenium.py
+++ lianxicode/test_start_selenium.py
@@ -3,8 +3,15 @@ from selenium import webdriver
 
 def test_start_selenium():
     driver = webdriver.Chrome()
-    url = 'http://www.baidu.com'
+    url = 'http://www.douban.com'
     driver.get(url)
 
+    input_el = driver.find_element('xpath', '//*[@id="anony-nav"]/div[3]/form/span[1]/input')
+    input_el.send_keys('老友记')
+    input_el.submit()
+
     # 断言
-    assert driver.title == '百度一下，你就知道'
+    actual = driver.title
+    # 关闭浏览器
+    driver.quit()
+    assert actual == '搜索: 老友记'
